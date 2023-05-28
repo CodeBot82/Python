d163966cd14cf4c8b96f1d553741d7510175fe4f
CodeBot82 <134826374+CodeBot82@users.noreply.github.com>
2023-05-28 01:06:47 -0400

    Add files via upload

---
+++ Ejer1.py
@@ -0,0 +1,12 @@
+#Ejercicio 1
+
+#Escribir un programa que pregunte el nombre del usuario en la consola y 
+# un número entero e imprima por pantalla en líneas distintas el nombre 
+# del usuario tantas veces como el número introducido.
+
+print("$Code_bot")
+
+nombre = input("Ingrese su nombre: ")
+n = input("Ingrese un numero: ")
+
+print((nombre + "\n") * int(n))
