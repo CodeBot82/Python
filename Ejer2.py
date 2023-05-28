aa40551058c423b20acdc8886d3d31abd62079da
CodeBot82 <134826374+CodeBot82@users.noreply.github.com>
2023-05-28 00:52:21 -0400

    Add files via upload

---
+++ Ejer2.py
@@ -0,0 +1,9 @@
+#Ejercicio 2
+
+#Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre 
+# por pantalla la frase invertida.
+
+print("$Code_bot")
+
+frase = input("Ingrese una frase: ")
+print(frase[::-1]) 
