72cc03d13228b07dfc07485d95d377a558d3ae50
CodeBot82 <134826374+CodeBot82@users.noreply.github.com>
2023-05-28 07:28:13 -0400

    Add files via upload

---
+++ Ejer4.py
@@ -0,0 +1,13 @@
+#Ejercicio 1
+
+#Escribir un programa que pregunte al usuario su edad y muestre por 
+# pantalla si es mayor de edad o no.
+
+print("$Code_bot")
+
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
